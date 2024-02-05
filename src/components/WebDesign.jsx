5cb817153b0251461b2c14e1befa8b811c94fcab
Tamby Mampionona <theriharson@email.com>
2024-02-05 21:22:06 +0300

    Ecran produit

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WebDesign = () => {
     return (
-        <div>Test</div>
+        <div>WebDesign</div>
     )
 }
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
+import ProductDesign from './ProductDesign'
+import Development from './Development'
+import WebDesign from './WebDesign'
 
 const data = [
   "Web Design",
@@ -75,17 +79,20 @@ const Right = styled.div`
 `
 
 const Works = () => {
+    const [work, setWork] = useState("Web Design")
     return (
         <Section>
           <Container>
             <Left> 
               <List>
                 {data.map((item) => (
-                  <ListItem key={item} text={item}>{item}</ListItem>
+                  <ListItem key={item} text={item} onClick={() => setWork(item)}>{item}</ListItem>
                 ))}
               </List>
             </Left>
-            <Right></Right>
+            <Right>
+              {work === "Web Design" ? (<WebDesign />) : work === "Development" ? (<Development />) : (<ProductDesign />)}
+            </Right>
           </Container>
         </Section>
     )
